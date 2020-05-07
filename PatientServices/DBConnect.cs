244f0629acf3ab64bc230553a1f814dbcbdbd8b1
avisona <38825748+avisona@users.noreply.github.com>
2020-05-07 02:57:05 -0500

    All

---
+++ PatientServices/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientServices
+{
+    public class DBConnect : DbContext
+    {
+        public DBConnect(DbContextOptions<DBConnect> options): base(options){}
+
+    }
+}
