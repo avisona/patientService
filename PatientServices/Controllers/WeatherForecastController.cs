1d264961422da587567019d5219c10c204dd3868
avisona <38825748+avisona@users.noreply.github.com>
2020-05-13 17:46:45 -0500

    V2

--- PatientServices/Controllers/WeatherForecastController.cs
+++ PatientServices/Controllers/WeatherForecastController.cs
@@ -35,5 +35,30 @@ namespace PatientServices.Controllers
             })
             .ToArray();
         }
+
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> WeatherGet(string name)
+        {
+            string ret = $"Your name { name}, Today is {DateTime.Today:D}\n" +
+                     "Today's hours of V2 leisure: " +
+                     $"{await GetLeisureHours()}";
+            return Ok(ret);
+        }
+
+        static async Task<int> GetLeisureHours()
+        {
+            // Task.FromResult is a placeholder for actual work that returns a string.
+            var today = await Task.FromResult<string>(DateTime.Now.DayOfWeek.ToString());
+
+            // The method then can process the result in some way.
+            int leisureHours;
+            if (today.First() == 'S')
+                leisureHours = 16;
+            else
+                leisureHours = 5;
+
+            return leisureHours;
+        }
     }
 }
